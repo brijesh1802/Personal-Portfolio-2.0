1efa3a5d8a011e328e66ffa65e4d91ee8ea2802b
Brijesh Poojary <brijeshpujari333@gmail.com>
2024-11-29 16:11:56 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/brijesh1802",
   resume:
-    "https://drive.google.com/file/d/1lO8zI8Lg3t_arkfBqRr_UhO1keaJEqrm/view?usp=sharing",
+    "https://drive.google.com/file/d/1GHa2EE7Xlfyfm7kCnZ_YLT37IKMXhWaw/view?usp=drivesdk",
   linkedin: "https://www.linkedin.com/in/brijesh-poojary-34090421b/",
   twitter: "https://x.com/brijesh_p3"
 };
